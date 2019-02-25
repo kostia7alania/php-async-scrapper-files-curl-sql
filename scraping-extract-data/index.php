5bb7ceec1d02218c6733ef8f3eec04ff8d630d44
kostia7alania <osetia-alania@mail.ru>
2019-02-25 16:54:29 +0300

    OLD SCHOOL VARIANT

--- scraping-extract-data/index.php
+++ scraping-extract-data/index.php
@@ -1,16 +1,101 @@
-<?php
-
+<?php 
 require __DIR__ . '/vendor/autoload.php';
 
+echo date('Y-m-d H:i:s');
 $loop = \React\EventLoop\Factory::create();
+$sel = '.menu_test>ul>li>a';
 $browser = new \Clue\React\Buzz\Browser($loop);
-$scraper = new \AsyncScraper\Scraper($browser);
+$scraper = new \AsyncScraper\Scraper($sel, $browser);
 
-$urls = [
-    'https://www.pexels.com/photo/adorable-animal-cat-cat-s-eyes-236603/',
-    'https://www.pexels.com/photo/adorable-animal-baby-blur-177809/',
-];
+// PDO ////// TRUNCATE TABLE [Regions]
+// автомат дата = ALTER TABLE Regions ADD CONSTRAINT DF_Regions DEFAULT GETDATE() FOR Date
 
-$scraper->scrape(...$urls)->then('print_r');
+/*  // Insert EXAMPLE: 
+$data = [
+    ['ЫЫЫЫ',  '===2'],
+    ['ЫЫЫЫЫ',    '===3'],
+]; //добавится ток первый, если Name  - ключ;
 
-$loop->run();
+ //INSERT: 
+    $sql = "INSERT INTO Regions ([name],[href]) VALUES (?,?)";
+    $data = $query->insert($sql, $data);
+*/
+/* //SELECT
+$sql = "SELECT * FROM [Regions] ORDER BY [Date]";
+$data = $query->select($sql);
+var_dump($data); die; */
+
+//$urls = [ 'http://www.morflot.ru/portyi_rf/reestr_mp.html' ];
+$urls = [ '/portyi_rf/reestr_mp.html' ];
+
+$s = $scraper
+    ->scrape(0, ...$urls)
+    ->then(function ($e) {
+        $ar = $e[0]->hrefs;
+        $ports_sql_array = array_map(function (array $obj) {  return [ $obj[0], $obj[1] ] ; }, $ar); //array => ['url1','url2'];
+
+        $sql = "INSERT INTO Regions ([name],[href]) VALUES (?,?)";
+        $query = new \AsyncScraper\Queries();
+        $data = $query->insert($sql, $ports_sql_array);
+
+        $urls2 = array_map(function (array $obj) { return $obj[1]; }, $ar); //array => ['url1','url2'];
+        
+        $loop = \React\EventLoop\Factory::create();
+        $sel = 'ul.docslist>li>a';
+        $browser = new \Clue\React\Buzz\Browser($loop);
+        $scraper = new \AsyncScraper\Scraper($sel, $browser);
+        $scraper->scrape(0, ...$urls2)->then(function ($e2) {//var_dump($e2[2]);die;
+
+            $ports_arrays = [];
+            foreach ($e2 as $obj) {
+                    foreach ($obj->hrefs as $obj2) {
+                        $port_name = $obj2[0];
+                        $port_href = $obj2[1]; // связываем порт с регионом
+                        preg_match('/\/\w*\/\w*\/\w*/', $port_href, $out);
+                        $regions_href = $out[0].'.html';
+                        $ports_arrays[] = [$port_name, $port_href, $regions_href];
+                    }
+            }
+            //var_dump($ports_arrays); die;//array (size=67) => ['url1','url2'];
+            
+            // ==> SQL 4 ports:
+            $ports_query= "INSERT INTO Ports ([name],[href],[regions_href]) VALUES (?,?,?)";
+            $query = new \AsyncScraper\Queries();
+            $data = $query->insert($ports_query, $ports_arrays);
+            
+            $urls3 = array_map(function (array $obj) {return $obj[1];}, $ports_arrays); //array => ['url1','url2'];
+            
+            //var_dump($urls3); //die;//array (size=67) [,,,]
+            echo '= urls3';
+            $loop = \React\EventLoop\Factory::create();
+            $sel = '.sbl>div>p';
+            $browser = new \Clue\React\Buzz\Browser($loop);
+            $scraper = new \AsyncScraper\Scraper($sel, $browser);
+            $scraper->scrape(0, ...$urls3)->then(function ($e3) {
+               // var_dump($e3); 
+               echo 666;
+                die;
+                
+                $docs_arrays = [];
+                foreach ($e3 as $obj) {
+                        foreach ($obj->hrefs as $obj2) {
+                            $port_name = $obj2[0];
+                            $port_href = $obj2[1]; // связываем порт с регионом
+                            //preg_match('/\/\w*\/\w*\/\w*/', $port_href, $out);
+                            //$regions_href = $out[0].'.html';
+                            $docs_arrays[] = [$port_name, $port_href];
+                        }
+                }
+            var_dump($docs_arrays); die;//array (size=67) => ['url1','url2'];
+
+            
+            });
+
+
+            echo 'out';
+
+        });
+        $loop->run();
+
+    });
+$loop->run(); 
